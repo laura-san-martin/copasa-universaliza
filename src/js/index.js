52281943be2b4239014e839482cc935e4d51876d
Laura San Martin <lauragabrielasan@gmail.com>
2023-10-17 17:40:52 -0300

    Ajustes do novo layout

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,45 @@ global.$ = $;
 global.jQuery = $;
 jQuery = $;
 import AOS from '../../node_modules/aos';
+import slick from '../../node_modules/slick-carousel';
 
 $("document").ready(function () {
 
     AOS.init();
 
     setElementsByScroll($(window).scrollTop());
+
+    $('.copasa__galeria').slick({
+        dots: false,
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ],
+        nextArrow: '<img src="./dist/img/icons/next.png" />',
+        prevArrow: '<img src="./dist/img/icons/prev.png" />',
+      });
 
     $('.navigation__icon').click(function(){
 		$('.navigation__menu').toggleClass('navigation__menu--open');
